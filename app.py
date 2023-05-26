676f28e4f0c9cdf776e719fe21b3092dde12910a
Ubuntu <ubuntu@ip-172-31-4-230.us-west-2.compute.internal>
2023-05-26 12:16:56 +0000

    codes

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import torch
 import torchvision
 
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
 import matplotlib.pyplot as plt
 import cv2
 
@@ -23,20 +26,29 @@ sam.to(device=device)
 
 mask_generator = SamAutomaticMaskGenerator(sam)
 
-image = cv2.imread('images/dog.jpg')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+@app.route('/sam', methods=['POST'])
+def upload():
+    
+    data = request.get_json()
+    image_base64 = data.get('image')
+    image_data = base64.b64decode(image_base64)
+    nparr = np.frombuffer(image_data, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-masks = mask_generator.generate(image)
+    masks = mask_generator.generate(image)
 
-plt.imshow(image)
-show_anns(masks)
-plt.axis('off')
-buffer = BytesIO()
-plt.savefig(buffer, format='jpg')
-buffer.seek(0)
+    plt.imshow(image)
+    show_anns(masks)
+    plt.axis('off')
+    buffer = BytesIO()
+    plt.savefig(buffer, format='jpg')
+    buffer.seek(0)
 
-# Encode the image as base64
-image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    # Encode the image as base64
+    image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
 
-# Print the base64 encoded image
-print(image_base64)
+    # Print the base64 encoded image
+    return jsonify({'image_base64': image_base64})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
