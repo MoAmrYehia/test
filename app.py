0d08f74ab5ac4bcd5ca8e6c00cf20f59f6e5b11b
Mohamed Amr <40462023+MoAmrYehia@users.noreply.github.com>
2023-05-26 14:17:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,17 @@
 import torch
 import torchvision
+
 import matplotlib.pyplot as plt
-import numpy as np
-import torch
 import cv2
 
 from io import BytesIO
 import base64
 
-def show_anns(anns):
-    if len(anns) == 0:
-        return
-    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
-    ax = plt.gca()
-    ax.set_autoscale_on(False)
-
-    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
-    img[:,:,3] = 0
-    for ann in sorted_anns:
-        m = ann['segmentation']
-        color_mask = np.concatenate([np.random.random(3), [0.35]])
-        img[m] = color_mask
-    ax.imshow(img)
-
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
-
 import sys
+
+from utlis import *
+
 sys.path.append("..")
 
 sam_checkpoint = "sam_vit_h_4b8939.pth"
